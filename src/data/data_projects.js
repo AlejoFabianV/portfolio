5a687cabcc763fab95f10258a8ab638df47e4dd0
alejo fabian <verhagenalejo@gmail.com>
2025-07-25 16:32:01 -0300

    nueva seccion en la data github

--- src/data/data_projects.js
+++ src/data/data_projects.js
@@ -3,22 +3,24 @@ export const data_projects = [
         id: 1,
         title: "Tienda digital",
         tecnologies: "React.js, Express.js, MySQL, CSS",
+        github: 'https://github.com/AlejoFabianV/Los-10-AnillosFinal',
         url: 'https://10-anillos.vercel.app'
     },
     {
         id: 2,
         title: "Manejador de tareas",
         tecnologies: "Nest.js, React.js, MongoDB, TailwindCSS",
+        github: "https://github.com/AlejoFabianV/tasksFront",
         url: 'https://task-manager-chi-indol.vercel.app/'
     },
-    {
-        id: 3,
-        title: "Tienda digital",
-        tecnologies: "JavaScript"
-    },
-    {
-        id: 4,
-        title: "Tienda digital",
-        tecnologies: "JavaScript"
-    },
+    // {
+    //     id: 3,
+    //     title: "Tienda digital",
+    //     tecnologies: "JavaScript"
+    // },
+    // {
+    //     id: 4,
+    //     title: "Tienda digital",
+    //     tecnologies: "JavaScript"
+    // },
 ]

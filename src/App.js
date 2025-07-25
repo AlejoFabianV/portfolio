f003afbab1f46b42d4f4e3aa64834ab3070d6e60
alejo fabian <verhagenalejo@gmail.com>
2025-07-25 16:20:45 -0300

    agregue la nueva seccion y nuevos estilos

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import Presentation from './components/pages/presentation';
 import AboutMe from './components/pages/aboutMe';
 import Projects from './components/pages/projects';
+import Technologies from './components/pages/technologies';
 import { data_projects } from './data/data_projects';
 
 
 function App() {
 
-  const projects = [...data_projects] //duplicado para que sea infinito
-
+  const projects = [...data_projects]
 
   return (
-    <section className=' bg-[#494343] min-h-screen'>
-      <Presentation />
-      <AboutMe />
-      
-      <div className=' mx-auto my-2 w-[90%]'>
-        <h1 className='text-3xl ml-16 my-4'>Proyectos</h1>
-        <div>
-          <div className='flex flex-wrap justify-center '>
-            {projects.map((project, index) => (
-              <Projects project={project} key={index} />
-              
-            ))}
+    <section className='  bg-[#F8EDE3] min-h-screen p-2'>
+      <div className=' bg-[#BDD2B6] border-2 border-[#A2B29F] max-w-[95%] mx-auto' >
+        <Presentation />
+        <AboutMe />
+        <Technologies />
+
+        <div className=' mx-auto my-2 w-[90%]'>
+          <h1 className='text-3xl ml-16 my-4 text-[#798777]'>Proyectos</h1>
+          <div>
+            <div className='flex flex-wrap justify-center '>
+              {projects.map((project, index) => (
+                <Projects project={project} key={index} />
+
+              ))}
+            </div>
           </div>
         </div>
       </div>
+
     </section>
   );
 }
 
 export default App;
+// bg-[#494343]
